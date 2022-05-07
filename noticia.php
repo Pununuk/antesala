ce8926cdbd698b96d3fb89b3659537fa0af1cf27
Pununuk <pununuk@MacBook-Pro-de-Pununuk.local>
2022-05-07 16:31:42 -0300

    subiendo noticia

---
+++ noticia.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Antesala | Podcast</title>
+        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico">
+
+        <!-- css -->
+        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/wow-master/css/animate.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+
+    </head>
+    <body>
+
+        <?php include 'includes/navbar.php';?> 
+
+        <main>
+            <section class="wow bounceInUp">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-8 col-md-9 pe-0">
+                            <div class="card-ticket cardLeft d-flex flex-column">
+                                <h1 class="fc-blanco">Nombre nota</h1>
+                                <div class="title">
+                                    <h2>Antesala</h2>
+                                    <span>sala</span>
+                                </div>
+                                <div>
+                                    <div class="seat">
+                                        <h2>156</h2>
+                                        <span>asiento</span>
+                                    </div>
+                                    <div class="time">
+                                        <h2>12:00</h2>
+                                        <span>hora</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-4 col-md-3 ps-0">
+                            <div class="card-ticket cardRight d-flex flex-column justify-content-around position-relative">
+                                <div class="text-center">
+                                    <img src="assets/img/iso-antesala.svg" alt="">
+                                </div>
+                                <div class="number">
+                                    <h3>156</h3>
+                                    <span>asiento</span>
+                                </div>
+                                <div class="text-center">
+                                    <img class="barchart" src="assets/img/barcode.svg" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section id="noticias" class="mt-100 pt-100 mb-5">
+                <div class="container">
+                    <div class="row mb-5">
+                        <div class="col-md-7 d-flex flex-column justify-content-center">
+                            <p>12 de enero de 2022</p>
+                            <hr class="hr-black">
+                            <h2 class="fc-amarillo">"El pucho en la boca"</h2>
+                            <p class="Grifter">Por Punu Vazquez</p>
+                            <strong>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis rerum sapiente accusantium, facilis labore exercitationem odio perferendis cum veniam quam!</strong>
+                            <strong>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis rerum sapiente accusantium, facilis labore exercitationem odio perferendis cum veniam quam!</strong>
+                        </div>
+                        <div class="col-md-5 text-md-end mt-4 mt-md-0">                  
+                            <img src="assets/img/img-multimedia.png" width="80%" alt="">
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="bg-gris-claro pt-100 pb-100">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia delectus expedita illo tenetur quasi non, ratione voluptates maiores quo modi reprehenderit nam aliquid accusamus perspiciatis dolorem quidem quae iusto dignissimos. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad enim eaque nobis, qui tenetur provident eos neque consequatur hic ipsam dicta distinctio? Distinctio eligendi esse, commodi reprehenderit saepe sequi suscipit.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia delectus expedita illo tenetur quasi non, ratione voluptates maiores quo modi reprehenderit nam aliquid accusamus perspiciatis dolorem quidem quae iusto dignissimos. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad enim eaque nobis, qui tenetur provident eos neque consequatur hic ipsam dicta distinctio? Distinctio eligendi esse, commodi reprehenderit saepe sequi suscipit.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia delectus expedita illo tenetur quasi non, ratione voluptates maiores quo modi reprehenderit nam aliquid accusamus perspiciatis dolorem quidem quae iusto dignissimos. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad enim eaque nobis, qui tenetur provident eos neque consequatur hic ipsam dicta distinctio? Distinctio eligendi esse, commodi reprehenderit saepe sequi suscipit.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia delectus expedita illo tenetur quasi non, ratione voluptates maiores quo modi reprehenderit nam aliquid accusamus perspiciatis dolorem quidem quae iusto dignissimos. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad enim eaque nobis, qui tenetur provident eos neque consequatur hic ipsam dicta distinctio? Distinctio eligendi esse, commodi reprehenderit saepe sequi suscipit.</p>
+                        </div>
+                        <div class="col-md-6">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia delectus expedita illo tenetur quasi non, ratione voluptates maiores quo modi reprehenderit nam aliquid accusamus perspiciatis dolorem quidem quae iusto dignissimos. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad enim eaque nobis, qui tenetur provident eos neque consequatur hic ipsam dicta distinctio? Distinctio eligendi esse, commodi reprehenderit saepe sequi suscipit.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia delectus expedita illo tenetur quasi non, ratione voluptates maiores quo modi reprehenderit nam aliquid accusamus perspiciatis dolorem quidem quae iusto dignissimos. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad enim eaque nobis, qui tenetur provident eos neque consequatur hic ipsam dicta distinctio? Distinctio eligendi esse, commodi reprehenderit saepe sequi suscipit.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia delectus expedita illo tenetur quasi non, ratione voluptates maiores quo modi reprehenderit nam aliquid accusamus perspiciatis dolorem quidem quae iusto dignissimos. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad enim eaque nobis, qui tenetur provident eos neque consequatur hic ipsam dicta distinctio? Distinctio eligendi esse, commodi reprehenderit saepe sequi suscipit.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia delectus expedita illo tenetur quasi non, ratione voluptates maiores quo modi reprehenderit nam aliquid accusamus perspiciatis dolorem quidem quae iusto dignissimos. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad enim eaque nobis, qui tenetur provident eos neque consequatur hic ipsam dicta distinctio? Distinctio eligendi esse, commodi reprehenderit saepe sequi suscipit.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            s
+            <?php include 'includes/subscription.php';?>
+        </main>
+        
+        <?php include 'includes/footer.php';?>
+
+        <!-- js -->
+        <script src="assets/jquery/jquery.min.js"></script>
+        <script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="assets/wow-master/js/wow.min.js"></script>
+        <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+        <script src="assets/js/custom.js"></script>
+    </body>
+</html>
